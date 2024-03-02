62b547ac0b1df9999c0a310efac53c2c73640723
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-03-02 20:06:46 +0300

    feat: add user and post

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,11 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLList, GraphQLObjectType, GraphQLSchema, Source, graphql } from 'graphql';
+import { User } from './user/user.js';
+import { Post } from './posts/post.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +16,29 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      return graphql({
+        schema: new GraphQLSchema({
+          query: new GraphQLObjectType({
+            name: 'Query',
+            fields: () => ({
+              users: {
+                type: new GraphQLList(User),
+                resolve: () => {
+                  return prisma.user.findMany();
+                },
+              },
+              posts: {
+                type: new GraphQLList(Post),
+                resolve: () => {
+                  return prisma.post.findMany();
+                },
+              },
+            }),
+          }),
+        }),
+        source: new Source(req.body.query),
+        variableValues: req.body.variables,
+      });
     },
   });
 };
---
+++ src/routes/graphql/posts/post.ts
@@ -0,0 +1,19 @@
+import { GraphQLObjectType } from 'graphql';
+import { GraphQLString } from 'graphql/index.js';
+import { UUIDType } from '../types/uuid.js';
+
+
+export const Post = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: {
+      type: UUIDType,
+    },
+    title: {
+      type: GraphQLString,
+    },
+    content: {
+      type: GraphQLString,
+    },
+  }),
+});
